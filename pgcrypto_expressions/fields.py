553fde70555cafd7be534c556c9cee4db5a0bbe4
Carl Meyer <carl@oddbird.net>
2015-05-20 23:30:38 -0600

    Add docstrings.

--- pgcrypto_expressions/fields.py
+++ pgcrypto_expressions/fields.py
@@ -2,6 +2,14 @@ from django.db import models
 
 
 class ByteArrayField(models.Field):
+    """A simple field type for the Postgres bytea type.
+
+    Represents values as bytearray instances in Python.
+
+    Mostly exists to facilitate testing of the Funcs in this package, but you
+    can certainly use it if you need it.
+
+    """
     def db_type(self, connection):
         return 'bytea'
 
--- pgcrypto_expressions/funcs.py
+++ pgcrypto_expressions/funcs.py
@@ -2,8 +2,20 @@ from django.db import models
 
 
 class PgpSymEncrypt(models.Func):
+    """The pgp_sym_encrypt function from pgcrypto.
+
+    Takes two arguments: a text value to be encrypted, and the encryption
+    key. Returns a bytea.
+
+    """
     function = 'pgp_sym_encrypt'
 
 
 class PgpSymDecrypt(models.Func):
+    """The pgp_sym_decrypt function from pgcrypto.
+
+    Takes two arguments: a bytea value to be decrypted, and the encryption
+    key. Returns text.
+
+    """
     function = 'pgp_sym_decrypt'
